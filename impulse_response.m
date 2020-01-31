dfb8b48331849ab52b88d5efaad63c709839b38b
Hunter Whaples <hwhaples@g.hmc.edu>
2020-01-30 21:31:12 -0800

    Removed unused file Qfcn.m and fixed error in impulse_response.m

--- impulse_response.m
+++ impulse_response.m
@@ -8,7 +8,8 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
+% Hunter Whaples, 1/30/2020
 [h,t] = tdomain(H,fs);
 
 end
